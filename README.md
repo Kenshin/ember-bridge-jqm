dcb139c36db60bb70b9d48276b6b7e5d11ccb6e3
kenshin <kenshin@ksria.com>
2012-09-03 11:42:36 +0800

    add files

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-ember-bridge-jqm
-================
+##Pagination Actionscript 3.0 Library
+基于Ember.js 0.9.8.1的jQuery Mobile“代理”插件
 
-Ember.js Bridge jQuery Mobile
+### 使用方法：  
+依赖：  
+<pre>
+&lt;script src=&quot;ember-0.9.8.1.min.js&quot;&gt;&lt;/script&gt;
+&lt;script src=&quot;jquery-1.8.0.min.js&quot;&gt;&lt;/script&gt;
+</pre>
+
+引入：  
+<pre>
+&lt;script src=&quot;ember-bridge-jqm.js&quot;&gt;&lt;/script&gt;
+</pre>
+
+
+使用（JavaScript）：  
+<pre>
+App.PageView = Mov.PageView.extend({
+    templateName:'main',
+    id: 'page-view',
+    didInsertElement: function() {
+        $.mobile.changePage(this.$());
+    }
+});
+App.HeaderView = Mov.HeaderView.extend();
+App.FooterView = Mov.FooterView.extend();
+App.ContentView = Mov.ContentView.extend();
+</pre>
+
+使用（HTML）：  
+<pre>
+&lt;script type=&quot;text/x-handlebars&quot; data-template-name=&quot;main&quot;&gt;&lt;/script&gt;
+
+	{{#view App.HeaderView}}
+		&lt;h1&gt;Adobe Reader&lt;/h1&gt;
+	{{/view}}
+
+	{{#view App.ContentView}}
+		{{#collection App.ListView contentBinding="App.getArticlesController"}}
+			&lt;a {{ bindAttr href="content.link" }} data-ajax="false" &gt;
+				&lt;h3&gt;{{ content.title }}&lt;/h3&gt;
+				&lt;p&gt;via {{ content.creator }}&lt;/p&gt;
+				&lt;p&gt;{{{ content.desc }}}&lt;/p&gt;
+			&lt;/a&gt;
+		{{/collection}}
+	{{/view}}
+
+	{{#view App.FooterView}}
+		&lt;h3&gt;via &lt;a href="http://www.k-zone.cn/zblog"&gt;Kenshin&lt;/a&gt;&lt;/h3&gt;
+	{{/view}}
+
+&lt;/script&gt;
+
+&lt;div data-role=&quot;page&quot;&gt;&lt;/div&gt;
+</pre>
+
+
+## 参考：
+* [https://github.com/LuisSala/emberjs-jqm](https://github.com/LuisSala/emberjs-jqm)
+* [https://github.com/tolbard/ember-moving](https://github.com/tolbard/ember-moving)
+
+## 更新日志：
+version 1.0 [2012-8-27]
+* 适合Ember.js 0.9.8.1与jQuery Mobile 1.2.0 alpha（及以下）版本
+* 适合jQuery Mobile单页面模式
+
+## 联系方式：
+* 博客：[k-zone.cn](http://www.k-zone.cn/zblog)
+* 微博：[新浪微博](http://weibo.com/23784148)
+* 联络：kenshin[AT]ksria.com
+
+## 版权和许可：
+Copyright 2012 [k-zone.cn](http://www.k-zone.cn/zblog)  
+Licensed under MIT or GPL Version 2 licenses
---
+++ ember-bridge-jqm.js
@@ -0,0 +1,258 @@
+﻿// Base classes for jQueryMobile Support
+// require emberjs, jquery, jquery mobile and the css file for all the previous 
+// librairy
+// this code is base of the work of LuisSala to see his code go to 
+// https://github.com/LuisSala/emberjs-jqm
+//------------------------------------------------
+// mix with jquery mobile
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  EmberMoving.MobileBaseView is just a basic class for all the commun attribut in
+  JQMobile.
+
+  Note: As of this writing, `MobileBaseView` only add some bidding to its 
+  superclass, `ArrayProxy`. The plan is to add additional all the commun
+  attribut (with the default valur)for all the jqm element.
+
+  @extends Ember.View
+*/
+Mov = EmberMoving = Ember.Namespace.create()
+
+	EmberMoving.MobileBaseView = Ember.View.extend({
+		attributeBindings:['data-role', 'data-theme'],
+		'data-theme': 'b'
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.PageView is the conterpart of the page element in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the page jqm element.
+
+  @extends EmberMoving.MobileBaseView
+*/
+
+	EmberMoving.PageView = EmberMoving.MobileBaseView.extend({
+		'data-role': 'page'
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.ToolbarBaseView is abstract and it's the conterpart of the toolbar
+  element in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the toolbar jqm element.
+
+  @extends EmberMoving.MobileBaseView
+*/
+
+	EmberMoving.ToolbarBaseView = EmberMoving.MobileBaseView.extend({
+		attributeBindings:['data-position'],
+		'data-position': function() {
+			if (this.get('isFullScreen')) {
+				return 'fullscreen'
+			}
+
+			if (this.get('isFixed')) {
+				return 'fixed'
+			}
+			return ''
+		}.property('isFixed', 'isFullScreen').cacheable(),
+
+		isFixed: true,
+		isFullsScreen: false
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.HeaderView is the conterpart of the toolbar header
+  element in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the header jqm element.
+
+  @extends EmberMoving.MobileBaseView
+*/
+
+	EmberMoving.HeaderView = EmberMoving.ToolbarBaseView.extend({
+		'data-role': 'header'
+
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.ContentView is the conterpart of the content element in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the content jqm element.
+
+  @extends EmberMoving.MobileBaseView
+*/
+
+	EmberMoving.ContentView = EmberMoving.MobileBaseView.extend({
+		'data-role': 'content'
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.FooterView is the conterpart of the toolbar Footer
+  element in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the Footer jqm element.
+
+  @extends EmberMoving.MobileBaseView
+*/
+
+	EmberMoving.FooterView = EmberMoving.MobileBaseView.extend({
+		'data-role': 'footer',
+		attributeBindings:['data-position'],
+		'data-position': function() {
+			if (this.get('isFixed')) {
+				return 'fixed'
+			}
+			return ''
+		}.property('isFixed').cacheable(),
+
+		isFixed: true
+	});
+
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.ListItemView is the conterpart of the element of a List View in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the item jqm element.
+
+  @extends Ember.View
+*/
+
+	EmberMoving.ListItemView = EmberMoving.MobileBaseView.extend({
+		tagName: 'li'/*,
+		classNames: [],
+		attributeBindings:['data-corners','data-shadow','data-inline','data-wrapperels','data-icon'],
+		'data-corners':'false',
+		'data-shadow':'false',
+		'data-iconshadow':'true',
+		'data-inline':'false',
+		'data-wrapperels':'div',
+		'data-icon':'arrow-r',
+		'data-iconpos':'right'*/
+
+	});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+  @class
+
+  Ember.ListView is the conterpart of the element of a List View in JQMobile.
+
+  Note: The plan is to add additional all the specific page attributs
+  attribut (with the default value)for the item jqm element.
+
+  @extends Ember.View
+*/
+
+	EmberMoving.ListView = Ember.CollectionView.extend({
+    attributeBindings: ['data-role', 'data-theme'],
+    'data-role':'listview',
+    tagName: 'ul',
+    itemViewClass: Ember.ListItemView,
+    init: function() {
+        this._super();
+        var _self = this;
+        //the 'later' is to avoid that the preload element of the associated Controler
+        //adopt the jqm style. Note : if you find a better way to do it that is compatible
+        // with all the browser just let me know (I have test IE, Chrome, IOS 5,
+        // black berry and Firefox)
+        //   Em.run.next and  Em.run.once don't work the daly is needed.
+        Em.run.next(function() {
+          _self.$().listview('refresh');
+        });
+        Em.run.later(function() {
+          _self.$().listview('refresh');
+        },500);
+    },
+    // Observe the attached content array's length and refresh the listview on the next RunLoop tick.
+    contentLengthDidChange: function(){
+      var _self = this;    
+      Em.run.next(function() {
+        _self.$().listview('refresh');
+      });
+      Em.run.later(function() {
+        _self.$().listview('refresh');
+      },1500);
+    }.observes('content.length')
+});
+
+// ==========================================================================
+// Project:  Ember - JQ Mobile
+// Copyright: ©2012 Tolbard and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+/**
+	helper: I am not sure why LuisSala have put this in his exemple
+	if you know put a comment to explain
+*/
+$(document).bind('mobileinit', function() {
+    $.mobile.touchOverflowEnabled = true;
+});
+
+$(document).bind( 'pageinit', function(){
+    var v = App.get( 'pageView' );
+    if ( !v ) {
+        v = App.PageView.create();
+        App.set( 'pageView',v );
+        v.append();
+    }
+});
